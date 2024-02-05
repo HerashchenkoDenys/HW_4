174fbf6934f5421c412026b58473a62a49145d1f
HerashchenkoDenys <157643762+HerashchenkoDenys@users.noreply.github.com>
2024-02-05 22:38:55 +0200

    Update HW_4.js

--- HW_4.js
+++ HW_4.js
@@ -1,1 +1,7 @@
-test
+number' + 3 + 3
+  
+//In JavaScript, when you use the + operator with a string and a number, it performs string concatenation. In the expression 'number' + 3 + 3, the first operation is the concatenation of the string 'number' with the number 3, resulting in the string 'number3'. Then, the next operation is the concatenation of this string with another number 3, resulting in the final string 'number33'.
+
+//So, the result of the expression 'number' + 3 + 3 in JavaScript is the string 'number33'.
+
+
