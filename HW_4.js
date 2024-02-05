1c65a09c1b681da62204de3025134771211ae3e1
HerashchenkoDenys <157643762+HerashchenkoDenys@users.noreply.github.com>
2024-02-05 23:16:06 +0200

    test5

--- HW_4.js
+++ HW_4.js
@@ -27,3 +27,19 @@ null + 3
 //The addition +'40' + +'2' adds these two numeric values together, resulting in 42.
 //Finally, the string "hillel" is concatenated to the numeric result, resulting in the string "42hillel".
 //So, the overall result of the expression +'40' + +'2' + "hillel" is the string "42hillel".
+
+'10' - 5 === 6;
+
+//Let's break down the expression '10' - 5 === 6 step by step:
+
+//'10' - 5: The subtraction operation (-) attempts to convert the string '10' to a number and then subtracts 5. The implicit conversion results in the numeric value 5 (because '10' as a number is 10, and subtracting 5 gives 5).
+//5 === 6: The strict equality operator (===) checks if the result from the previous operation (which is 5) is strictly equal to the number 6. In this case, the result is false because 5 is not equal to 6.
+//So, the overall result of the expression '10' - 5 === 6 is false.
+
+true + false
+
+//In JavaScript, when you use the + operator with boolean values, they are implicitly converted to numbers before the addition operation. The boolean value true is converted to 1, and false is converted to 0.
+
+//So, true + false is equivalent to 1 + 0, which results in the numeric value 1.
+
+//Therefore, the result of true + false in JavaScript is 1.
