79816de587ad72cd162050a65de3a263ad6c138f
HerashchenkoDenys <157643762+HerashchenkoDenys@users.noreply.github.com>
2024-02-05 23:05:17 +0200

    Update HW_4.js

--- HW_4.js
+++ HW_4.js
@@ -9,3 +9,21 @@ null + 3
 //In JavaScript, when you use the + operator with null and a number, null is treated as 0 during the operation. Therefore, the result of the expression null + 3 will be 3. This is because adding 0 to any number does not change the value of that number.
 
 //So, the result of null + 3 in JavaScript is 3.
+
+ 5 && "qwerty"
+
+
+//In JavaScript, the && (logical AND) operator returns the value of the second operand if the first operand is truthy; otherwise, it returns the value of the first operand.
+
+//In the expression 5 && "qwerty", both operands are truthy. In a logical AND operation, if both operands are truthy, the result is the value of the second operand. Therefore, the result of this expression is the string "qwerty".
+
++'40' + +'2' + "hillel";
+
+
+Let's break down the expression '40' + '2' + "hillel" step by step:
+
++'40': The unary plus (+) before the string '40' converts it to a number, resulting in the numeric value 40.
++'2': Similarly, the unary plus before the string '2' converts it to a number, resulting in the numeric value 2.
+The addition +'40' + +'2' adds these two numeric values together, resulting in 42.
+Finally, the string "hillel" is concatenated to the numeric result, resulting in the string "42hillel".
+So, the overall result of the expression +'40' + +'2' + "hillel" is the string "42hillel".
