7b0412f408a001eca95f44a668e2bcdc96410905
HerashchenkoDenys <157643762+HerashchenkoDenys@users.noreply.github.com>
2024-02-05 22:40:58 +0200

    Update HW_4.js

--- HW_4.js
+++ HW_4.js
@@ -4,4 +4,8 @@ number' + 3 + 3
 
 //So, the result of the expression 'number' + 3 + 3 in JavaScript is the string 'number33'.
 
+null + 3
 
+//In JavaScript, when you use the + operator with null and a number, null is treated as 0 during the operation. Therefore, the result of the expression null + 3 will be 3. This is because adding 0 to any number does not change the value of that number.
+
+//So, the result of null + 3 in JavaScript is 3.
